7973ac923d335bcb7e5c1e6262e35d2213f965ea
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-03-20 16:12:22 -0600

    service worker modify

--- serviceworker.js
+++ serviceworker.js
@@ -1,20 +1,33 @@
 const cache_name = "pwa_cv_kevin";
-const urlsToCache = [
-    '/', 'js/index.js', "/index.html"
-];
-
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(cache_name).then(cache => {
-            cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+            '/index.js'
+        ]);
+    })());
 });
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open(cache_name);
+
+        // Get the resource from the cache.
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            return cachedResponse;
+        } else {
+            try {
+                // If the resource was not in the cache, try the network.
+                const fetchResponse = await fetch(event.request);
+
+                // Save the resource in the cache and return it.
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            } catch (e) {
+                // The network failed.
+            }
+        }
+    })());
 });
