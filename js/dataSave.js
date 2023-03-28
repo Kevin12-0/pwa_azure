c7e69771d11e839d056024b62a2bb5a40aa55101
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-03-27 23:25:27 -0600

    changes for localstorage

--- js/dataSave.js
+++ js/dataSave.js
@@ -1,4 +1,18 @@
-"use strict"
+/* 
+    guardar datos en navegaror con funcion anonima
+*/
+const dataSave = async () => {
+    const coment = document.getElementById('comentText');
+    let comentText = coment.value;
+    /* obtener el nombre y vercion de un navegador */
+    const navigator = bowser.name;
+    const version = bowser.version;
+    const objectData = {
+        navigator: navigator,
+        coment: comentText
+    }
+    console.log(comentText);
+    console.log(`${navigator} ${version}`);
+    console.log(localStorage.setItem("navegador", `$navigator $version`));
 
-const  suabaseURL = "";
-const suabaseKEY = "";
+}
